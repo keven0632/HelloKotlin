ea38b85e39e39286df20fa705c90dd892427c43b
zhengjian <keven0632@gmail.com>
2017-10-23 22:54:50 +0800

    mac版本提交

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,6 +1,8 @@
 /**
  * @author KeVeN
  * @date 2017/10/20 14:58
+ *
+ * Mac上Idea实用的时候，快捷键打开设置Command+,
  */
 fun main(args: Array<String>) {
     print("Hello Kotlin!开始学习Kotlin")
---
+++ src/集合类型-set.kt
@@ -0,0 +1,20 @@
+/**
+ * @author KeVeN
+ * @from  Mac
+ * @date 2017/10/23 下午10:46
+ * 集合类型 Set：无序不重复
+ * 主要方法：交叉并补
+ */
+fun main(args: Array<String>) {
+   //Set<类型>或setof（元素1，元素2...）
+   //大小固定，元素类型不可变
+
+
+    //元素计数：count（），空否：isEmpty()
+
+    //检查是否包含某个元素：contains，包含另一个Set：contaionsAll
+
+    //转化为数组：toTypedArray()
+
+    //集合之间的运算：intersect/subtract/union/minus(补集的被操作对象无需是Set类型)
+}
